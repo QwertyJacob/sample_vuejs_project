fe473a6c20e9035d722271dfa123393034c0ad9d
j.cevallosmoreno <jesusfcevallos@gmail.com>
2022-12-06 09:12:21 +0100

    Step 1 -> No classes:
    - we see a lot of variables, if we would need to create other instruments, then we should create for each one of those another loop maybe, with another pace, maybe other notes, etc...
    
    - classes are used to wrap functionalities into an abstract objects. By doing it, we can handle easily more instances of a same object.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Classes And Inheritance</h1>
+  <button @click="startTone">Press to start</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  created(){
+
+    // we create an instrument:
+    this.monoSynth = new this.$tone.Synth();
+    this.monoSynth.toDestination();
+
+
+    // how many notes per loop?
+    this.notes_per_loop = 4;
+    // we create a loop function:
+    this.myLoop = new this.$tone.Loop(this.loopCallback, String(this.notes_per_loop)+"n");
+
+    // let us get some notes
+    this.scale = "C4 major";
+    this.notes = this.$tonal.Scale.get(this.scale).notes;
+
+
+
+  },
+  methods:{
+    startTone(){
+      this.$tone.start().then( () => {
+        this.$tone.Transport.start();
+        console.log("Tone started!");
+        this.myLoop.start();
+      });
+    },
+    loopCallback(transportTime){
+      let note_index = Math.floor(Math.random() * this.notes.length )
+      console.log("note ", this.notes[note_index], 'at time', transportTime)
+
+      this.monoSynth.triggerAttackRelease(this.notes[note_index], String(this.notes_per_loop)+"n");
+
+    }
+
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+import * as Vue from 'vue'
 import App from './App.vue'
+import * as Tone from 'tone'
+import p5 from "p5";
+import * as Tonal from 'tonal'
 
-createApp(App).mount('#app')
+const app = Vue.createApp(App)
+app.config.globalProperties.$tone = Tone;
+app.config.globalProperties.$p5 = p5;
+app.config.globalProperties.$tonal = Tonal;
+
+app.mount('#app')
+
+
